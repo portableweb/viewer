c82bc66e74247de23d5144638a1025fc80564831
Omprakash Selvaraj <cse.omprakash@gmail.com>
2026-07-24 08:04:52 -0400

    viewers stub versions

--- src-tauri/src/main.rs
+++
@@ -1,6 +0,0 @@
-// Prevents a console window from appearing on Windows in release builds.
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-
-fn main() {
-    portableweb_viewer_lib::run();
-}
